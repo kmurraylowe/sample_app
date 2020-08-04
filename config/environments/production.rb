5f9226e061c1667e2be1c646e72ca79b179c8dc6
Ubuntu <ubuntu@ip-172-31-41-153.us-east-2.compute.internal>
2020-08-04 00:33:09 +0000

    sendgrid api

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,8 +13,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      =>  ENV['SENDGRID_USERNAME'],
-    :password       =>  ENV['SENDGRID_PASSWORD'],
+    :user_name      =>  'kmurraylowe@gmail.com',
+    :password       =>  'kye18jay!',
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
