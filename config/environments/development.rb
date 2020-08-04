bb4c4713a00ef513c9e2550b9b5035d19808ca2b
Ubuntu <ubuntu@ip-172-31-41-153.us-east-2.compute.internal>
2020-08-04 00:04:25 +0000

    11.4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  host = 'a9ccde4a1dc14a2b8f029414a1bf9d78.vfs.cloud9.us-east-2.amazonaws.com/'
+  host = '0dd5aff5207a440e9b95801a8e31981f.vfs.cloud9.us-east-2.amazonaws.com/'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   
   config.action_mailer.perform_caching = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,14 +7,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'calm-everglades-44420.herokuapp.com'
+  host = 'boiling-falls-26463.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      =>  'app175038120@heroku.com',           #ENV['SENDGRID_USERNAME'],
-    :password       =>  'w2z2erpb4269',                   #ENV['SENDGRID_PASSWORD'],
+    :user_name      =>  ENV['SENDGRID_USERNAME'],
+    :password       =>  ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
